b1eb464df87a765d3f42d6deaf18e49ea0f1a1ca
Pietro Bonaldo Gregori <pietro.bogre@gmail.com>
2024-02-06 12:19:42 +0100

    reduce number of lsblk calls

--- get-data.sh
+++ get-data.sh
@@ -5,25 +5,28 @@
 
 echo -n > data/disks
 
-disk_blks=$(lsblk -o NAME,TYPE | awk '$2=="disk" {print $1}')
+lsblk=$(lsblk -o TYPE,FSTYPE,FSUSED,FSSIZE,MOUNTPOINT,NAME,NAME -b)
+
+disk_blks=$( echo "$lsblk" | awk '$1=="disk" {print "/dev/"$2}')
 for i in $disk_blks;
 do
-    smart=$(smartctl -iA /dev/$i)
+    smart=$(smartctl -iA $i)
+
     model_family=$(echo "$smart" | grep "Model Family:" | sed 's/^Model Family:\s*\(\S.*\)$/\1/g')
     serial_number=$(echo "$smart" | grep "Serial Number:" | sed 's/^Serial Number:\s*\(\S.*\)$/\1/g')
     rotation_rate=$(echo "$smart" | grep "Rotation Rate:" | sed 's/^Rotation Rate:\s*\(\S.*\)$/\1/g')
 
     smart_attributes=$(echo "$smart" | awk '/ID#\s*ATTRIBUTE_NAME\s*/{flag=1} /SMART Error Log Version:/{flag=0} flag')
 
-    power_on_hours=$(echo "$smart_attributes" | awk '$2=="Power_On_Hours" {print $10}')
-    power_cycle_count=$(echo "$smart_attributes" | awk '$2=="Power_Cycle_Count" {print $10}')
-    raw_read_error_rate=$(echo "$smart_attributes" | awk '$2=="Raw_Read_Error_Rate" {print $10}')
-    temperature=$(echo "$smart_attributes" | awk '$2=="Temperature_Celsius" {print $10}')
+    power_on_hours=$(echo "$smart" | awk '$2=="Power_On_Hours" {print $10}')
+    power_cycle_count=$(echo "$smart" | awk '$2=="Power_Cycle_Count" {print $10}')
+    raw_read_error_rate=$(echo "$smart" | awk '$2=="Raw_Read_Error_Rate" {print $10}')
+    temperature=$(echo "$smart" | awk '$2=="Temperature_Celsius" {print $10}')
    
     echo "/dev/"$i",$model_family,$serial_number,$rotation_rate,$power_on_hours,$power_cycle_count,$raw_read_error_rate,$temperature" >> data/disks
 done
 
-parts_info=$(lsblk -o TYPE,FSTYPE,FSUSED,FSSIZE,MOUNTPOINT,NAME,NAME -b | awk '$1=="part" {print "/dev/"$7","$5","$2","$3","$4}')
+parts_info=$(echo "$lsblk" | awk '$1=="part" {print "/dev/"$7","$5","$2","$3","$4}')
 echo "$parts_info" > data/parts
 
 
