f305d6f83b0e2079613f739d23e9a72619754d2b
Pietro Bonaldo Gregori <pietro.bogre@gmail.com>
2024-02-06 01:42:56 +0100

    create collector class to allow continuous metrics updates

--- serve-data.py
+++ serve-data.py
@@ -29,43 +29,45 @@ def parse_csv():
 
     yield parts
 
-def collect(disks, parts):
-    disk_labels = ['block']
-    disk_model = Info('disk_model', 'Disk Model Family', disk_labels)
-    disk_power_on_hours = Gauge('disk_power_on_hours', 'Hours spent with disk powered', disk_labels)
-    disk_power_cycle_count = Gauge('disk_power_cycle_count', 'Disk power cycle count', disk_labels)
-    disk_raw_read_error_rate = Gauge('disk_raw_read_error_rate', 'Disk raw read error rate', disk_labels)
-    disk_temperature = Gauge('disk_temperature', 'Disk temperature in Celsius', disk_labels)
+class Collector:
+    def __init__(self):
+        disk_labels = ['block']
+        self.disk_model = Info('disk_model', 'Disk Model Family', disk_labels)
+        self.disk_power_on_hours = Gauge('disk_power_on_hours', 'Hours spent with disk powered', disk_labels)
+        self.disk_power_cycle_count = Gauge('disk_power_cycle_count', 'Disk power cycle count', disk_labels)
+        self.disk_raw_read_error_rate = Gauge('disk_raw_read_error_rate', 'Disk raw read error rate', disk_labels)
+        self.disk_temperature = Gauge('disk_temperature', 'Disk temperature in Celsius', disk_labels)
 
-    part_labels = ['block', 'disk']
-    part_info = Info('partition', 'Partition metadata information', part_labels)
-    part_usage_bytes = Gauge('partition_usage_bytes', 'Partition used size in bytes', part_labels)
-    part_size_bytes = Gauge('partition_size_bytes', 'Partition total size in bytes', part_labels)
+        part_labels = ['block', 'disk']
+        self.part_info = Info('partition', 'Partition metadata information', part_labels)
+        self.part_usage_bytes = Gauge('partition_usage_bytes', 'Partition used size in bytes', part_labels)
+        self.part_size_bytes = Gauge('partition_size_bytes', 'Partition total size in bytes', part_labels)
 
-    # Disk metrics
-    for disk in disks:
-        disk_model.labels(block=disk[0]).info({
-            'model_family': disk[1],
-            'serial': disk[2],
-            'rpm': disk[3]
-        })
-        disk_power_on_hours.labels(block=disk[0]).set(disk[4])
-        disk_power_cycle_count.labels(block=disk[0]).set(disk[5])
-        disk_raw_read_error_rate.labels(block=disk[0]).set(disk[6])
-        disk_temperature.labels(block=disk[0]).set(disk[7])
+    def collect(self, disks, parts):    
+        # Disk metrics
+        for disk in disks:
+            self.disk_model.labels(block=disk[0]).info({
+                'model_family': disk[1],
+                'serial': disk[2],
+                'rpm': disk[3]
+            })
+            self.disk_power_on_hours.labels(block=disk[0]).set(disk[4])
+            self.disk_power_cycle_count.labels(block=disk[0]).set(disk[5])
+            self.disk_raw_read_error_rate.labels(block=disk[0]).set(disk[6])
+            self.disk_temperature.labels(block=disk[0]).set(disk[7])
 
-    # Partition metrics
-    for part in parts:
-        part_info.labels(block=part[0], disk=part[5]).info({
-            'mountpoint': part[1],
-            'filesystem': part[2]
-        })
-        part_usage_bytes.labels(block=part[0], disk=part[5]).set(part[3])
-        part_size_bytes.labels(block=part[0], disk=part[5]).set(part[4])
+        # Partition metrics
+        for part in parts:
+            self.part_info.labels(block=part[0], disk=part[5]).info({
+                'mountpoint': part[1],
+                'filesystem': part[2]
+            })
+            self.part_usage_bytes.labels(block=part[0], disk=part[5]).set(part[3])
+            self.part_size_bytes.labels(block=part[0], disk=part[5]).set(part[4])
 
 if __name__ == '__main__':
     disks, parts = parse_csv()
-    collect(disks, parts)
+    collector = Collector()
 
     # disable default metrics
     prometheus_client.REGISTRY.unregister(prometheus_client.GC_COLLECTOR)
@@ -75,3 +77,5 @@ if __name__ == '__main__':
     start_http_server(3112)
     while True:
         time.sleep(1)
+        disks,parts = parse_csv()
+        collector.collect(disks, parts)
